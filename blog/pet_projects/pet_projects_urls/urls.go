c27f1037e1df4048e0cfa3ea8c85cc93c5045c00
dd84ai <dd84ai@gmail.com>
2024-01-28 23:40:08 +0100

    feat: adding typelog into projects

--- blog/pet_projects/pet_projects_urls/urls.go
+++ blog/pet_projects/pet_projects_urls/urls.go
@@ -1,6 +1,8 @@
 package pet_projects_urls
 
 const (
-	Autogit = "https://github.com/darklab8/darklab_autogit"
-	Blog    = "https://github.com/darklab8/darklab_blog"
+	Autogit   = "https://github.com/darklab8/darklab_autogit"
+	Blog      = "https://github.com/darklab8/darklab_blog"
+	GoTypelog = "https://github.com/darklab8/go-typelog"
+	PyTypelog = "https://github.com/darklab8/py-typelog"
 )
