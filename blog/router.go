594243b75ae582a73e83095baae35124f0a65cdd
dd84ai <dd84ai@gmail.com>
2024-02-10 21:57:54 +0100

    feat: mermaid.js support

--- blog/router.go
+++ blog/router.go
@@ -6,6 +6,7 @@ import (
 	"darklab_blog/blog/articles/article_detailed"
 	"darklab_blog/blog/common/builder"
 	"darklab_blog/blog/common/urls"
+	"darklab_blog/blog/examples"
 	"darklab_blog/blog/favourite"
 	"darklab_blog/blog/pet_projects"
 	"darklab_blog/blog/pet_projects/ideas"
@@ -37,6 +38,10 @@ func init() {
 		builder.NewComponent(
 			urls.Articles,
 			articles.HomeT(),
+		),
+		builder.NewComponent(
+			"examples",
+			examples.Example(),
 		),
 	)
 	for _, idea := range ideas.Ideas {
