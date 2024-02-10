db90fc547594eae8343b973e770ef3431495f2f1
dd84ai <dd84ai@gmail.com>
2024-02-10 03:46:16 +0100

    refactor: archived url links

--- blog/archive/links.go
+++ blog/archive/links.go
@@ -1,7 +1,25 @@
 package archive
 
-var LinkTypeSafety = NewLink(
-	"http://www.pl-enthusiast.net/2014/08/05/type-safety/",
-	"what_is_type_safety_ThePLEnthusiast.html",
-	"what_is_type_safety_ThePLEnthusiast.pdf",
+var (
+	LinkTypeSafety = NewLink(
+		"http://www.pl-enthusiast.net/2014/08/05/type-safety/",
+		"what_is_type_safety_ThePLEnthusiast.html",
+		"what_is_type_safety_ThePLEnthusiast.pdf",
+	)
+	LinkGrugBrainDev = NewLink(
+		"https://grugbrain.dev/",
+		"The_Grug_Brained_Developer.html",
+		"The_Grug_Brained_Developer.pdf",
+	)
+
+	LinkLocality = NewLink(
+		"https://htmx.org/essays/locality-of-behaviour/",
+		"LocalityOfBehaviour.html",
+		"LocalityOfBehaviour.pdf",
+	)
+	LinkPlatformsRant = NewLink(
+		"https://gist.github.com/chitchcock/1281611",
+		"PlatformsRant.html",
+		"PlatformsRant.pdf",
+	)
 )
