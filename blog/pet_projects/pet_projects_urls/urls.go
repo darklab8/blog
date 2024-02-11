d0b6acd382408b45ddb0002bac7e2b40ea24068f
dd84ai <dd84ai@gmail.com>
2024-02-11 16:46:31 +0100

    refactor: wrong link to blog

--- blog/pet_projects/pet_projects_urls/urls.go
+++ blog/pet_projects/pet_projects_urls/urls.go
@@ -1,8 +1,8 @@
 package pet_projects_urls
 
 const (
-	Autogit   = "https://github.com/darklab8/darklab_autogit"
-	Blog      = "https://github.com/darklab8/github.com/darklab8/blog"
+	Autogit   = "https://github.com/darklab8/autogit"
+	Blog      = "https://github.com/darklab8/blog"
 	GoTypelog = "https://github.com/darklab8/go-typelog"
 	PyTypelog = "https://github.com/darklab8/py-typelog"
 )
