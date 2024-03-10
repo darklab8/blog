18b8fb6b31d78a22cde25c9bd17d04ae52005cfb
dd84ai <dd84ai@gmail.com>
2024-03-10 05:19:53 +0100

    docs: add mention of infra repo

--- blog/pet_projects/pet_projects_urls/urls.go
+++ blog/pet_projects/pet_projects_urls/urls.go
@@ -5,4 +5,5 @@ const (
 	Blog      = "https://github.com/darklab8/blog"
 	GoTypelog = "https://github.com/darklab8/go-typelog"
 	PyTypelog = "https://github.com/darklab8/py-typelog"
+	Infra     = "https://github.com/darklab8/infra"
 )
