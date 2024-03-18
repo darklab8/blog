b119ee16cfad0a449e09cb1253d85a0b929e1253
dd84ai <dd84ai@gmail.com>
2024-03-18 07:56:34 +0100

    docs: mention project argocd-cue

--- blog/pet_projects/pet_projects_urls/urls.go
+++ blog/pet_projects/pet_projects_urls/urls.go
@@ -6,4 +6,5 @@ const (
 	GoTypelog = "https://github.com/darklab8/go-typelog"
 	PyTypelog = "https://github.com/darklab8/py-typelog"
 	Infra     = "https://github.com/darklab8/infra"
+	Argocue   = "https://github.com/darklab8/argocd-cue"
 )
