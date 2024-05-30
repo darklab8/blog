54596ce8e5f7ce211eaff1b9675ea9e53fde701b
dd84ai <dd84ai@gmail.com>
2024-05-30 11:55:30 +0200

    refactor: about page to be first

--- blog/common/urls/urls.go
+++ blog/common/urls/urls.go
@@ -1,12 +1,12 @@
 package urls
 
 const (
-	Articles = "index.html"
+	Articles = "articles.html"
 
 	ArticleGitConventionalCommits = "article/git_conventional_commits.html"
 	ArticleTypelog                = "article/typelog_type_safe_structured_logging.html"
 
-	About = "about.html"
+	About = "index.html"
 
 	PetProjects      = "pet_projects.html"
 	PetProjectsIdeas = "pet_projects_ideas.html"
