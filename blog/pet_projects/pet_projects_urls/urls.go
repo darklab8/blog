5a31c831291c9bb887d9b12e3fe4e91f673c5b60
dd84ai <dd84ai@gmail.com>
2025-01-25 22:19:05 +0100

    docs: mention freelancer server

--- blog/pet_projects/pet_projects_urls/urls.go
+++ blog/pet_projects/pet_projects_urls/urls.go
@@ -1,10 +1,11 @@
 package pet_projects_urls
 
 const (
-	Autogit   = "https://github.com/darklab8/autogit"
-	Blog      = "https://github.com/darklab8/blog"
-	GoTypelog = "https://github.com/darklab8/go-typelog"
-	PyTypelog = "https://github.com/darklab8/py-typelog"
-	Infra     = "https://github.com/darklab8/infra"
-	Argocue   = "https://github.com/darklab8/argocd-cue"
+	Autogit         = "https://github.com/darklab8/autogit"
+	Blog            = "https://github.com/darklab8/blog"
+	GoTypelog       = "https://github.com/darklab8/go-typelog"
+	PyTypelog       = "https://github.com/darklab8/py-typelog"
+	Infra           = "https://github.com/darklab8/infra"
+	Argocue         = "https://github.com/darklab8/argocd-cue"
+	FlServerVanilla = "https://github.com/darklab8/fl-server-vanilla"
 )
